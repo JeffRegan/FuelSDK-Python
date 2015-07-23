cc11ab2cee0d970bbee80bec37f241b84eaf58b2
Jeff Regan <jregan@whoat.net>
2015-07-23 10:37:22 -0700

    Updated config.py
    Made it optional to use env variables for app signature, default wsdl, auth url, and wsdl file location.

--- FuelSDK/config.py
+++ FuelSDK/config.py
@@ -2,12 +2,16 @@ import os
 
 
 class ExactTargetConfig(object):
-    app_signature = None
+    '''
+    client_id and client_secret are required
+    other params have defaults and are therefore optional
+    '''
     client_id = os.environ['FUELSDK_CLIENT_ID']
     client_secret = os.environ['FUELSDK_CLIENT_SECRET']
-    default_wsdl = 'https://webservice.exacttarget.com/etframework.wsdl'
-    authentication_url = 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
-    wsdl_file_local_loc = '/tmp/ExactTargetWSDL.s6.xml'
+    app_signature = os.environ.get('FUELSDK_APP_SIGNATURE', None)
+    default_wsdl = os.environ.get('FUELSDK_DEFAULT_WSDL', 'https://webservice.exacttarget.com/etframework.wsdl')
+    authentication_url = os.environ.get('FUELSDK_AUTH_URL', 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1')
+    wsdl_file_local_loc = os.environ.get('FUELSDK_WSDL_FILE_LOCAL_LOC', '/tmp/ExactTargetWSDL.s6.xml')
 
 
 exact_target_config = ExactTargetConfig()
